879d2b4b8667466f23a11b945b41e33f46b0bb5d
Lancercraft <2023149@student.cct.ie>
2023-10-10 12:35:37 +0100

    Hello World
    added a print Statement to ptint "Hello World"

--- src/sdfgit/SDFGit.java
+++ src/sdfgit/SDFGit.java
@@ -14,9 +14,8 @@ public class SDFGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         
-        
+        System.out.println("Hello World");
         
         
         
